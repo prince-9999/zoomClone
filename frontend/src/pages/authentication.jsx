707db4908effcabca3b2bb5e16914c6c668ae206
prince <princeprajapati3848@gmail.com>
2024-12-05 18:35:18 +0530

    deploying frontend

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -12,7 +12,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { AuthContext } from '../contexts/AuthContext';
+import { AuthContext } from '../contexts/authContext';
 import { Snackbar } from '@mui/material';
 
 
