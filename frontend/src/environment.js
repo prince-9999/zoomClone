cafb86b642b2b60508ab33778bd31cfec1b07eeb
prince <princeprajapati3848@gmail.com>
2024-12-06 13:00:33 +0530

    changes

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 
-const server =IS_PROD ?
-    "http://localhost:8000":
-    "https://zoomclonebackend-p7dh.onrender.com"
+const server = IS_PROD ?
+    "https://zoomclonebackend-p7dh.onrender.com" :
+    "http://localhost:8000"
 
 
 export default server;
